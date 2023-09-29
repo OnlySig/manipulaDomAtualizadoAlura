001a298aa23a46d48702c64ee7e69f5fcfc0968f
OnlySig <gabriel.castro.pereira11@gmail.com>
2023-09-29 19:58:44 -0300

    add: temporizador only console.log

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 const btnDescanso = document.querySelectorAll(".app__card-list-item")
 const bgHtml = document.querySelector("html")
+const btnComecar = document.querySelector("#start-pause")
 const imgFundo = document.querySelector(".app__image")
 const appTitle = document.querySelector(".app__title")
 const inputToggleSwitch = document.querySelector("#alternar-musica")
 const musica = new Audio('./sons/luna-rise-part-one.mp3')
+const audioPlay = new Audio("./sons/play.wav")
+const audioPause = new Audio("./sons/pause.mp3")
+const audioEnd = new Audio("./sons/beep.mp3")
+
 musica.loop = true
+
+let tempoDecorridoEmSegundos = 5
+let intervaloId = null
 
 inputToggleSwitch.addEventListener("change", _ => {
     if(musica.paused) {
@@ -44,4 +52,32 @@ function alterarTitle(h1, h2) {
             <strong class="app__title-strong">${h2}</strong>
         </h1>
     `
+}
+
+const contagemRegressiva = () => {
+    if(tempoDecorridoEmSegundos <= 0) {
+        zerar()
+        audioEnd.play()
+        alert("acabou!")
+        return
+    }
+    tempoDecorridoEmSegundos -= 1
+    console.log(`Temporizador: ${tempoDecorridoEmSegundos}`)
+}
+
+btnComecar.addEventListener("click", iniciarOuPausar)
+
+function iniciarOuPausar() {
+    if(intervaloId) {
+        zerar()
+        audioPause.play()
+        return
+    }
+    audioPlay.play()
+    intervaloId = setInterval(contagemRegressiva, 1000)
+}
+
+function zerar() {
+    clearInterval(intervaloId)
+    intervaloId = null
 }
